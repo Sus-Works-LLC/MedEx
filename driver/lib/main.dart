44fe54370569fa5582c97f332916e0e70af6e8b5
Aneesh <aneeshyr2@gmail.com>
2023-06-09 17:44:48 +0530

    logged in screen, background execution, websocket conn

--- driver/lib/main.dart
+++ driver/lib/main.dart
@@ -1,9 +1,136 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'map.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
   runApp(const MyApp());
 }
 
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  /// OPTIONAL, using custom notification channel id
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'my_foreground', // id
+    'MY FOREGROUND SERVICE', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.low, // importance must be at low or higher level
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+
+      notificationChannelId: 'my_foreground',
+      initialNotificationTitle: 'AWESOME SERVICE',
+      initialNotificationContent: 'Initializing',
+      foregroundServiceNotificationId: 888,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+    ),
+  );
+
+  service.startService();
+}
+
+void onStart(ServiceInstance service) async {
+  // Only available for flutter 3.0.0 and later
+
+  // For flutter prior to version 3.0.0
+  // We have to register the plugin manually
+
+  /// OPTIONAL when use custom notification
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  InitializationSettings initset = const InitializationSettings(android: const AndroidInitializationSettings("mipmap/ic_launcher"));
+  flutterLocalNotificationsPlugin.initialize(initset);
+  // flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation< AndroidFlutterLocalNotificationsPlugin>().requestPermission();
+
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      print("setasfg");
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      print("setasbg");
+      service.setAsBackgroundService();
+    });
+  }
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  final _channel = WebSocketChannel.connect(
+    Uri.parse('ws://10.60.210.126:5000/tests/ws'),
+  );
+  var url = Uri.http('10.60.210.126:5000', 'login');
+  final player = AudioPlayer();
+
+  service.on("send").listen((event) {
+    print("instructed to send message $event");
+    _channel.sink.add("$event");
+  });
+
+  service.on("login").listen((event) {
+    print("logging in");
+    var postbody = json.encode({
+      "phone":  1234567890,
+      "password": "susybaka"
+    });
+    var resp = http.post(url, headers: {"Content-Type": "application/json"}, body: postbody);
+  });
+
+  service.on("stopsound").listen((event) async {
+    await player.stop();
+  });
+
+  _channel.stream.listen((event) async {
+    flutterLocalNotificationsPlugin.show(
+        888,
+        'AMBULANCE REQUEST ASSIGNED',
+        'Click to open app',
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'my_foreground',
+            'MY FOREGROUND SERVICE',
+            icon: 'ic_bg_service_small',
+            ongoing: false,
+            playSound: false,
+          ),
+        ),
+      );
+      await player.setUrl("asset:assets/tele.mp3");
+      await player.play();
+  });
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,15 +140,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +149,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,6 +157,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  
 
   void _incrementCounter() {
     setState(() {
@@ -59,35 +168,14 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Padding(
@@ -114,10 +202,16 @@ class _MyHomePageState extends State<MyHomePage> {
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                  
+
                 ),
                 onPressed: () {
-            
+                  final service = FlutterBackgroundService();
+                  service.invoke("send", {"hello": "world"});
+                  service.invoke("login");
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoggedinPage())
+                  );
                 }, 
                 child: const Text(
                   "Login",
@@ -127,7 +221,51 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class LoggedinPage extends StatefulWidget {
+  const LoggedinPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoggedinPage> createState() => _LoggedinPageState();
+}
+
+
+class _LoggedinPageState extends State<LoggedinPage> {
+  String amb = "Waiting for request";
+
+  final service = FlutterBackgroundService();
+
+  void ambreq() {
+    setState(() {
+      amb = "Ambulance has been requested";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("You are logged in"),
+      ),
+      body:
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(onPressed: () {
+                MapUtils.openMap(12.294481608813694, 76.62807500565476);
+                final service = FlutterBackgroundService();
+                service.invoke("stopsound");
+              }, icon: const Icon(Icons.location_on_rounded), iconSize: 100.0,),
+              const Text("Ambulance has been requested!"),
+            ],
+          ),
+        ),
     );
   }
 }
